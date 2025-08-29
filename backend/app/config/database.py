08d3c6ee033ef6f6e9eb1ff2226ea77efb30ed0a
sheheryarghaznavi <sheheryarghaznavi@yahoo.com>
2025-08-29 09:04:00 +0500

    database config file added

---
+++ backend/app/config/database.py
@@ -0,0 +1,22 @@
+from functools import lru_cache
+from pydantic_settings import BaseSettings
+from pydantic import Field
+
+
+class DB(BaseSettings):
+
+    # Database
+    db_host: str = Field(default="db")
+    db_port: int = Field(default=3306)
+    db_user: str = Field(default="sprintsync")
+    db_password: str = Field(default="sprintsync")
+    db_name: str = Field(default="sprintsync")
+
+    class Config:
+        env_file = ".env"
+        case_sensitive = False
+        extra='ignore'
+
+@lru_cache
+def getDB():
+    return DB()
