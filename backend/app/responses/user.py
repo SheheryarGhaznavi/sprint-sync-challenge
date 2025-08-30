ff844f77f76fce07f891070b3a54c0978ba0260a
sheheryarghaznavi <sheheryarghaznavi@yahoo.com>
2025-08-30 18:02:22 +0500

    another user response added

--- backend/app/responses/user.py
+++ backend/app/responses/user.py
@@ -1,15 +1,21 @@
 from sqlmodel import SQLModel
-from typing import List
+from typing import List, Union
 
 
 class UserRead(SQLModel):
     id: int
     email: str
     is_admin: bool
-    
-    
+
+
 class UsersListResponse(SQLModel):
     error: int
     message: str
     data: List[UserRead]
+
+
+class UserResponse(SQLModel):
+    error: int
+    message: str
+    data: Union[UserRead, List] = []
 
