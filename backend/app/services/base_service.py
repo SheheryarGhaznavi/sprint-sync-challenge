3316bb68fdd06507f1182b203b85173da27b8190
sheheryarghaznavi <sheheryarghaznavi@yahoo.com>
2025-08-30 16:13:08 +0500

    base service class added with some error handling

---
+++ backend/app/services/base_service.py
@@ -0,0 +1,23 @@
+
+class BaseService:
+
+
+    async def callFunction(self, function_name, params = []):
+
+        error = 0
+        message = "success"
+        data = []
+
+        try:
+            function = getattr(self, function_name)
+            data = await function(**params)
+
+        except AttributeError:
+            error = 1
+            message = f"Function {function_name} not found"
+
+        except Exception as e:
+            error = 1
+            message = f"Error executing function {function_name}: {e}"
+        
+        return { "error": error, "message": message, "data": data }
