5820201be3b12ffcb1938877b7850a63cb48a458
sheheryarghaznavi <sheheryarghaznavi@yahoo.com>
2025-08-30 15:24:21 +0500

    env_file path updated in database confif file

--- backend/app/config/database.py
+++ backend/app/config/database.py
@@ -1,6 +1,7 @@
 from functools import lru_cache
 from pydantic_settings import BaseSettings
 from pydantic import Field
+import os
 
 
 class DB(BaseSettings):
@@ -13,7 +14,7 @@ class DB(BaseSettings):
     db_name: str = Field(default="sprintsync")
 
     class Config:
-        env_file = ".env"
+        env_file = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), ".env")
         case_sensitive = False
         extra='ignore'
 
