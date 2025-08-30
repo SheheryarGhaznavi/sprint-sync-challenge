1c07561bb0bda6cc1e334754efa2203d03005f13
sheheryarghaznavi <sheheryarghaznavi@yahoo.com>
2025-08-30 15:21:38 +0500

    table name added in User model

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,9 +1,11 @@
 from typing import Optional
 from sqlmodel import SQLModel, Field
-import re
 
 
 class User(SQLModel, table=True):
+
+    __tablename__ = "users"
+
     id: Optional[int] = Field(default=None, primary_key=True)
     email: str = Field(index=True, unique=True, max_length=50)
     hashed_password: str = Field(max_length=50)
