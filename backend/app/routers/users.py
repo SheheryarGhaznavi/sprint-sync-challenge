3436449da5fb3cb909c24d07248d4f1b19c18048
sheheryarghaznavi <sheheryarghaznavi@yahoo.com>
2025-08-31 16:04:31 +0500

    users routes can only be access by admin user

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -4,6 +4,8 @@ from fastapi import APIRouter, Depends
 from app.services.user_service import UserService
 from app.responses.user import UsersListResponse, UserResponse
 from app.requests.user import UserRequest
+from app.utils.deps import getCurrentAdmin
+from app.models.user import User
 
 router = APIRouter()
 
@@ -11,33 +13,33 @@ router = APIRouter()
 
 ## List Users Route
 @router.get("/", response_model = UsersListResponse)
-async def listUsers( service: UserService = Depends(UserService) ):
+async def listUsers( service: UserService = Depends(UserService), admin: User = Depends(getCurrentAdmin) ):
     return await service.callFunction("list", {"skip": 0, "limit": 10})
 
 
 
 ## Create User Route
 @router.post("/", response_model = UserResponse)
-async def createUser( data: UserRequest, service: UserService = Depends(UserService) ):
+async def createUser( data: UserRequest, service: UserService = Depends(UserService), admin: User = Depends(getCurrentAdmin) ):
     return await service.callFunction("create", {"user": data})
 
 
 
 ## Get User Route
 @router.get("/{user_id}", response_model = UserResponse)
-async def getUser( user_id: int, service: UserService = Depends(UserService) ):
+async def getUser( user_id: int, service: UserService = Depends(UserService), admin: User = Depends(getCurrentAdmin) ):
     return await service.callFunction("getById", {"user_id": user_id})
 
 
 
 ## Update User Route
 @router.put("/{user_id}")
-async def updateUser( user_id: int, data: UserRequest, service: UserService = Depends(UserService) ):
+async def updateUser( user_id: int, data: UserRequest, service: UserService = Depends(UserService), admin: User = Depends(getCurrentAdmin) ):
     return await service.callFunction("update", {"user_id": user_id, "user_data": data})
 
 
 
 ## Delete User Route
 @router.delete("/{user_id}")
-async def deleteUser( user_id: int, service: UserService = Depends(UserService) ):
+async def deleteUser( user_id: int, service: UserService = Depends(UserService), admin: User = Depends(getCurrentAdmin) ):
     return await service.callFunction("delete", {"user_id": user_id})
