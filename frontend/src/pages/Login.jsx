6a74013237328efa21a859e59b557444125b0ec7
sheheryarghaznavi <sheheryarghaznavi@yahoo.com>
2025-09-01 17:26:29 +0500

    if user is logged in then moved him to home page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -16,6 +16,15 @@ class Login extends Component
     }
 
 
+    componentDidMount() {
+        const token = localStorage.getItem('token')
+        const user = localStorage.getItem('user')
+        if (token && user) {
+            window.location.href = '/'
+        }
+    }
+
+
     handleSubmit = async (e) => {
 
         e.preventDefault()
