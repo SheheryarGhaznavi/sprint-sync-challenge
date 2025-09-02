734f9bd6661b32182ea67e6bcf5b420598db6a87
sheheryarghaznavi <sheheryarghaznavi@yahoo.com>
2025-09-02 12:04:51 +0500

    base url updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000'
+const baseURL = import.meta.env.VITE_API_BASE_URL || 'https://sprintsync-backend-production.up.railway.app/'
 const api = axios.create({ baseURL })
 
 api.interceptors.request.use(config => {
